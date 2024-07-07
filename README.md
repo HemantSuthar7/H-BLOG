c48980fbf5f133b60c3a88105ad16e6dc0ca38ce
HemantSuthar7 <hemantsuthar9324@gmail.com>
2024-07-07 16:08:48 +0530

    Created project structure and services

---
+++ README.md
@@ -0,0 +1,3 @@
+# H-BLOG :A Mega Blog with Appwrite
+
+## This project is currently under development by Hemant Suthar
---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1>H-BLOG :A Mega Blog with Appwrite</h1>
+      <h3>This project is under development by Hemant Suthar</h3>
+      
+    </>
+  )
+}
+
+export default App
