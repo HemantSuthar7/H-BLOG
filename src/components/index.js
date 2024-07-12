90bd6a467f49a065195ce4db7829c5faa4d04781
HemantSuthar7 <hemantsuthar9324@gmail.com>
2024-07-12 12:51:57 +0530

    added login, postcard & select components and completed their logic

--- src/components/index.js
+++ src/components/index.js
@@ -3,11 +3,15 @@ import Footer from "./Footer/Footer.jsx";
 import Container from "./Container/Container.jsx";
 import LogoutBtn from "./Header/LogoutBtn.jsx";
 import Logo from "./Logo.jsx";
+import Input from "./Input.jsx";
+import Button from "./Button.jsx"
 
 export {
     Header,
     Footer,
     Container,
     LogoutBtn,
-    Logo
+    Logo,
+    Input,
+    Button
 }
