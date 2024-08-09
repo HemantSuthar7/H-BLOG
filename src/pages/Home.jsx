aa5c3cacd86ebff031bd0e1d5c5a75763dac13e1
HemantSuthar7 <hemantsuthar9324@gmail.com>
2024-08-09 21:19:14 +0530

    made some changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,39 +1,6 @@
-import React, {useEffect, useState} from 'react'
-import { Container, PostCard } from "../components"
-// import appwriteService from "../appwrite/config"
+import React from 'react'
 
 function Home() {
-
-    // const [posts, setPosts] = useState([]);
-
-    // useEffect( () => {
-    //     appwriteService.getPosts()
-    //     .then( (posts) => {
-    //         if (posts) {
-    //             setPosts(posts.documents)
-    //         }
-    //     } )
-    //     .catch((error)=>{
-    //         console.log("Did not got posts. -from Home page.     The Error is : ", error);
-    //     })
-    // }, [] )
-
-    // if (posts.length === 0) {
-    //     return (
-    //         <div className="w-full py-8 mt-4 text-center">
-    //             <Container>
-    //                 <div className="flex flex-wrap">
-    //                     <div className="p-2 w-full">
-    //                         <h1 className="text-2xl font-bold hover:text-gray-500">
-    //                             Login to read posts
-    //                         </h1>
-    //                     </div>
-    //                 </div>
-    //             </Container>
-    //         </div>
-    //     )
-    // }
-
 
   return (
     <div className='max-w-full py-8 min-h-screen'>
